3cc343e97514cf277fede7854d31158b0c1ce5b5
BERKYT <BerkyT115@yandex.ru>
2024-02-29 14:26:55 +0300

    refactor

--- src/services/admin_panel/panel.py
+++ src/services/admin_panel/panel.py
@@ -8,43 +8,48 @@ _model_collector = ModelCollector()
 
 
 @_model_collector.target_model()
-class UserAdmin(ModelView, model=User):
+class _UserTable(ModelView, model=User):
     column_list = [User.id, User.name]
 
 
 @_model_collector.target_model()
-class StudentAdmin(ModelView, model=Student):
+class _StudentTable(ModelView, model=Student):
     column_list = [Student.id, Student.name]
 
 
 @_model_collector.target_model()
-class DayAdmin(ModelView, model=Day):
+class _DayTable(ModelView, model=Day):
     column_list = [Day.date]
 
 
 @_model_collector.target_model()
-class AttendAdmin(ModelView, model=Attend):
+class _AttendTable(ModelView, model=Attend):
     column_list = [Attend.id, Attend.user_id, Attend.date_v]
 
 
 @_model_collector.target_model()
-class GradingAdmin(ModelView, model=Grading):
+class _GradingTable(ModelView, model=Grading):
     column_list = [Grading.id, Grading.subj_id, Grading.mark, Grading.theme]
 
 
 @_model_collector.target_model()
-class MessageQueueAdmin(ModelView, model=MessageQueue):
+class _MessageQueueTable(ModelView, model=MessageQueue):
     column_list = [MessageQueue.id, MessageQueue.message]
 
 
 @_model_collector.target_model()
-class SubjectAdmin(ModelView, model=Subject):
+class _SubjectTable(ModelView, model=Subject):
     column_list = [Subject.id, Subject.name]
 
 
 @_model_collector.target_model()
-class PlatoonAdmin(ModelView, model=Platoon):
+class _PlatoonTable(ModelView, model=Platoon):
     column_list = [Platoon.platoon_number]
+
+
+@_model_collector.target_model()
+class _AdminTable(ModelView, model=Admin):
+    column_list = [Admin.name]
 
 
 ADMIN_MODELS = _model_collector.models
