1eca5f2c7d92fe929b30324be3cbb9deebdc7790
BERKYT <BerkyT115@yandex.ru>
2024-03-04 20:19:58 +0300

    refactor

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,4 @@
 # Эти импорты заполняют переменную Base, в которой лежат метаданные, это нужно, чтобы модели были видны при миграциях
-from sqlalchemy.orm import relationship
-
 from models.student import Student
 from models.admin import Admin
 from models.day import Day
@@ -11,9 +9,3 @@ from models.user import User
 from models.platoon import Platoon
 from models.schedule import Schedule
 from models.subject import Subject
-
-# Day.schedule = relationship("Schedule", back_populates="day")
-# Attend.user = relationship("User", back_populates="attend")
-#
-# Schedule.day = relationship("Day", back_populates="schedule")
-# Schedule.platoon = relationship("Platoon")
